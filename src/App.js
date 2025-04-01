006613e7e7d5b90bde96c034aeff9514459a797b
Kayla W <wagne285@uwm.edu>
2025-04-01 09:32:15 -0500

    updated main app with BGST info

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo"  */}
+        <h2>
+          Board Game Statistic Tracker
+        </h2>
+        <p>Demo User Logged In</p>
+        <h3>Components</h3>
+
+          <li>Search Board Game Database</li>
+          <li>Add Board Game Session to Play History</li>
+          <li>View Play History</li>
+          <li>View Stats</li>
+
       </header>
     </div>
   );
